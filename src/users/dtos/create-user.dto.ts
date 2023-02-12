c837c6ba2b4967b665e7678fc0c350810de7adbb
Zephyr.Jeon <zephyr.jeon@odcode.com>
2023-02-11 20:21:58 -0800

    create user api implemented

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,10 +1,6 @@
 import { IsEmail, IsString } from 'class-validator';
-import { PrimaryGeneratedColumn } from 'typeorm';
 
 export class CreateUserDto {
-  @PrimaryGeneratedColumn()
-  id: number;
-
   @IsEmail()
   email: string;
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // InjectRepository tells the dependency injection system that this repository is needed
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
