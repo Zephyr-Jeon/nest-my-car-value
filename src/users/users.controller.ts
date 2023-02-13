54011ff25dfe2e11d54d8dcb2d1373ea8eb1dce6
Zephyr.Jeon <zephyr.jeon@odcode.com>
2023-02-12 16:43:08 -0800

    implement  global intercepter to attach current user to request context

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,9 +12,11 @@ import {
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user-dto';
 import { UserDto } from './dtos/user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 // @UseInterceptors(new SerializerInterceptor(UserDto))
@@ -47,8 +49,11 @@ export class UsersController {
   }
 
   @Get('/currentuser')
-  currentUser(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  // currentUser(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+  currentUser(@CurrentUser() user: User) {
+    return user;
   }
 
   @Get('/:id')
