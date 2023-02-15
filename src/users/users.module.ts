140496a7b90621d876c1dea81499a3d229bb1336
Zephyr.Jeon <zephyr.jeon@odcode.com>
2023-02-14 16:50:54 -0800

    add admin guard and change current user interceptor to middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+// import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -13,10 +14,16 @@ import { UsersService } from './users.service';
   providers: [
     UsersService,
     AuthService,
-    {
-      provide: APP_INTERCEPTOR, // Globally apply a specific intercepter
-      useClass: CurrentUserInterceptor,
-    },
+    // {
+    //   provide: APP_INTERCEPTOR, // Globally apply a specific intercepter
+    //   useClass: CurrentUserInterceptor,
+    // },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  // use CurrentUserMiddleware instead of CurrentUserInterceptor
+  // since Guards run after Middlewares and before Interceptors
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
